0a4b424cd46638c9b1c8433d60b75280a531309f
Knut Nesheim <knut.nesheim@wooga.com>
2011-05-04 12:07:41 +0200

    Whitespace cleanup.

--- src/s3.erl
+++ src/s3.erl
@@ -1,7 +1,7 @@
 %%%-------------------------------------------------------------------
 %%% File    : s3.erl
 %%% Author  : Andrew Birkett <andy@nobugs.org>
-%%% Description : 
+%%% Description :
 %%%
 %%% Created : 14 Nov 2007 by Andrew Birkett <andy@nobugs.org>
 %%%-------------------------------------------------------------------
@@ -9,7 +9,7 @@
 
 %% API
 -export([ list_buckets/0, create_bucket/1, delete_bucket/1,
-	  list_objects/2, list_objects/1, write_object/4, write_object/5, read_object/2, delete_object/2,
+      list_objects/2, list_objects/1, write_object/4, write_object/5, read_object/2, delete_object/2,
           set_credentials/1]).
 
 -export([s3Host/0]).
@@ -30,13 +30,13 @@ create_bucket (Name) -> do_put(Name).
 delete_bucket (Name) -> do_delete(Name).
 list_buckets ()      -> do_listbuckets().
 
-write_object (Bucket, Key, Data, ContentType) -> 
+write_object (Bucket, Key, Data, ContentType) ->
     write_object (Bucket, Key, Data, ContentType, undefined).
-write_object (Bucket, Key, Data, ContentType, ACL) -> 
+write_object (Bucket, Key, Data, ContentType, ACL) ->
     do_put(Bucket, Key, Data, ContentType, ACL).
-read_object (Bucket, Key) -> 
+read_object (Bucket, Key) ->
     do_get(Bucket, Key).
-delete_object (Bucket, Key) -> 
+delete_object (Bucket, Key) ->
     do_delete(Bucket, Key).
 
 %% option example: [{delimiter, "/"},{maxresults,10},{prefix,"/foo"}]
@@ -45,7 +45,7 @@ list_objects (Bucket) -> list_objects(Bucket, []).
 
 
 
-						% Bucket operations
+                        % Bucket operations
 do_listbuckets() ->
     xmlToBuckets(getRequest( "", "", [] )).
 
@@ -54,18 +54,18 @@ do_put(Bucket) ->
     ok.
 
 do_delete(Bucket) ->
-    try 
-	{_Headers,_Body} = deleteRequest( Bucket, ""),
-	ok
+    try
+    {_Headers,_Body} = deleteRequest( Bucket, ""),
+    ok
     catch
-	throw:X -> X
+    throw:X -> X
     end.
 
-						% Object operations
+                        % Object operations
 do_put(Bucket, Key, Content, ContentType, ACL) ->
     {Headers,_Body} = putRequest( Bucket, Key, Content, ContentType, ACL),
     [ETag | _] = [V || {K, V} <- Headers,
-		       string:to_lower(K) == "etag"],
+               string:to_lower(K) == "etag"],
     {ok, ETag}.
 
 do_list(Bucket, Options) ->
@@ -77,11 +77,11 @@ do_get(Bucket, Key) ->
     getRequest( Bucket, Key, [] ).
 
 do_delete(Bucket, Key) ->
-    try 
-	{_Headers,_Body} = deleteRequest( Bucket, Key),
-	ok
+    try
+    {_Headers,_Body} = deleteRequest( Bucket, Key),
+    ok
     catch
-	throw:X -> X
+    throw:X -> X
     end.
 
 %%--------------------------------------------------------------------
@@ -91,20 +91,20 @@ do_delete(Bucket, Key) ->
 s3Host () ->
     "s3.amazonaws.com".
 
-option_to_param( { prefix, X } ) -> 
+option_to_param( { prefix, X } ) ->
     { "prefix", X };
-option_to_param( { maxkeys, X } ) -> 
+option_to_param( { maxkeys, X } ) ->
     { "max-keys", integer_to_list(X) };
-option_to_param( { delimiter, X } ) -> 
+option_to_param( { delimiter, X } ) ->
     { "delimiter", X }.
 
 getRequest( Bucket, Key, Params ) ->
     genericRequest( get, Bucket, Key, Params, [], <<>>, "" ).
 putRequest( Bucket, Key, Content, ContentType, ACL) ->
     Headers = case ACL of
-		  [_ | _] -> [{"x-amz-acl", ACL}];
-		  _ -> []
-	      end,
+          [_ | _] -> [{"x-amz-acl", ACL}];
+          _ -> []
+          end,
     genericRequest( put, Bucket, Key, [], Headers, Content, ContentType ).
 deleteRequest( Bucket, Key ) ->
     genericRequest( delete, Bucket, Key, [], [], <<>>, "" ).
@@ -114,10 +114,10 @@ isAmzHeader( Header ) -> lists:prefix("x-amz-", Header).
 
 canonicalizedAmzHeaders( AllHeaders ) ->
     AmzHeaders = [ {string:to_lower(K),V} || {K,V} <- AllHeaders, isAmzHeader(K) ],
-    Strings = lists:map( 
-		fun s3util:join/1, 
-		s3util:collapse( 
-		  lists:keysort(1, AmzHeaders) ) ),
+    Strings = lists:map(
+        fun s3util:join/1,
+        s3util:collapse(
+          lists:keysort(1, AmzHeaders) ) ),
     s3util:string_join( lists:map( fun (S) -> S ++ "\n" end, Strings), "").
 
 canonicalizedResource ( "", "" ) -> "/";
@@ -129,22 +129,22 @@ stringToSign ( Verb, ContentMD5, ContentType, Date, Bucket, Path, OriginalHeader
     s3util:string_join( Parts, "\n") ++ canonicalizedResource(Bucket, Path).
 
 sign (Key,Data) ->
-						%    io:format("Data being signed is ~p~n", [Data]),
+                        %    io:format("Data being signed is ~p~n", [Data]),
     binary_to_list( base64:encode( crypto:sha_mac(Key,Data) ) ).
 
 queryParams( [] ) -> "";
-queryParams( L ) -> 
+queryParams( L ) ->
     Stringify = fun ({K,V}) -> K ++ "=" ++ V end,
     "?" ++ s3util:string_join( lists:map( Stringify, L ), "&" ).
 
 buildHost("") -> s3Host();
 buildHost(Bucket) -> Bucket ++ "." ++ s3Host().
 
-buildUrl(Bucket,Path,QueryParams) -> 
+buildUrl(Bucket,Path,QueryParams) ->
     ibrowse_lib:parse_url("http://" ++ buildHost(Bucket) ++ "/" ++ Path ++ queryParams(QueryParams)).
 
 buildContentHeaders( <<>>, _ ) -> [];
-buildContentHeaders( Contents, ContentType ) -> 
+buildContentHeaders( Contents, ContentType ) ->
     [{"Content-Length", integer_to_list(size(Contents))},
      {"Content-Type", ContentType}].
 
@@ -154,45 +154,45 @@ genericRequest( Method, Bucket, Path, QueryParams, UserHeaders, Contents, Conten
     Url = buildUrl(Bucket,Path,QueryParams),
 
     OriginalHeaders = [{"Connection", "keep-alive"}] ++
-	buildContentHeaders( Contents, ContentType ) ++
-	UserHeaders,
+    buildContentHeaders( Contents, ContentType ) ++
+    UserHeaders,
     ContentMD5 = "",
     Body = Contents,
 
     {ok, #aws_credentials{ accessKeyId=AKI, secretAccessKey=SAK }} = s3pool:get_credentials(),
 
     Signature = sign( SAK,
-		      stringToSign( MethodString, ContentMD5, ContentType, 
-				    Date, Bucket, Path, OriginalHeaders )),
+              stringToSign( MethodString, ContentMD5, ContentType,
+                    Date, Bucket, Path, OriginalHeaders )),
 
     Headers = [ {"Authorization","AWS " ++ AKI ++ ":" ++ Signature },
-		{"Host", buildHost(Bucket) },
-		{"Date", Date } 
-		| OriginalHeaders ],
+        {"Host", buildHost(Bucket) },
+        {"Date", Date }
+        | OriginalHeaders ],
 
-						%    io:format("Sending request ~p~n", [Request]),
+                        %    io:format("Sending request ~p~n", [Request]),
     Options = case Method of
-		  put ->
-		      [{content_type, ContentType}];
-		  _ ->
-		      [{response_format, binary}]
-	      end,
+          put ->
+              [{content_type, ContentType}];
+          _ ->
+              [{response_format, binary}]
+          end,
     Reply = attempt(
-	      fun() ->
-		      {ok, HttpClient} = s3pool:get_worker(),
-		      ibrowse_http_client:send_req(HttpClient, 
-						   Url, Headers, Method,
-						   Body, Options,?TIMEOUT )
-	      end, ?RETRIES),
-						%    io:format("HTTP reply was ~p~n", [Reply]),
+          fun() ->
+              {ok, HttpClient} = s3pool:get_worker(),
+              ibrowse_http_client:send_req(HttpClient,
+                           Url, Headers, Method,
+                           Body, Options,?TIMEOUT )
+          end, ?RETRIES),
+                        %    io:format("HTTP reply was ~p~n", [Reply]),
     case Reply of
-	{ok, Code, ResponseHeaders, ResponseBody } 
-	  when Code=="200"; Code=="204"
-	       -> 
-	    {ResponseHeaders,ResponseBody};
+    {ok, Code, ResponseHeaders, ResponseBody }
+      when Code=="200"; Code=="204"
+           ->
+        {ResponseHeaders,ResponseBody};
 
-	{ok, _HttpCode, _ResponseHeaders, ResponseBody } -> 
-	    throw ( parseErrorXml(ResponseBody) )
+    {ok, _HttpCode, _ResponseHeaders, ResponseBody } ->
+        throw ( parseErrorXml(ResponseBody) )
     end.
 
 
@@ -200,18 +200,18 @@ parseBucketListXml (Xml) ->
     {XmlDoc, _Rest} = xmerl_scan:string( binary_to_list(Xml) ),
     ContentNodes = xmerl_xpath:string("/ListBucketResult/Contents", XmlDoc),
 
-    GetObjectAttribute = fun (Node,Attribute) -> 
-				 [Child] = xmerl_xpath:string( Attribute, Node ),
-				 {Attribute, s3util:string_value( Child )}
-			 end,
+    GetObjectAttribute = fun (Node,Attribute) ->
+                 [Child] = xmerl_xpath:string( Attribute, Node ),
+                 {Attribute, s3util:string_value( Child )}
+             end,
 
     NodeToRecord = fun (Node) ->
-			   #object_info{ 
-			 key =          GetObjectAttribute(Node,"Key"),
-			 lastmodified = GetObjectAttribute(Node,"LastModified"),
-			 etag =         GetObjectAttribute(Node,"ETag"),
-			 size =         GetObjectAttribute(Node,"Size")}
-		   end,
+               #object_info{
+             key =          GetObjectAttribute(Node,"Key"),
+             lastmodified = GetObjectAttribute(Node,"LastModified"),
+             etag =         GetObjectAttribute(Node,"ETag"),
+             size =         GetObjectAttribute(Node,"Size")}
+           end,
     { ok, lists:map( NodeToRecord, ContentNodes ) }.
 
 parseErrorXml (Xml) ->
@@ -229,12 +229,12 @@ xmlToBuckets( {_Headers,Body} ) ->
 
 attempt(F, Retries) ->
     case (catch F()) of
-	{_Error, Reason} when Retries > 0 ->
+    {_Error, Reason} when Retries > 0 ->
         error_logger:error_msg("Error in reading from S3: ~w. Wait: ~w mseg, retry no: ~w~n",
                                [Reason, abs(Retries - ?RETRIES) * ?RETRY_DELAY, abs(Retries - ?RETRIES) + 1]),
 
-	    timer:sleep(abs(Retries - ?RETRIES) * ?RETRY_DELAY),
-	    attempt(F, Retries - 1);
-	R ->
-	    R
+        timer:sleep(abs(Retries - ?RETRIES) * ?RETRY_DELAY),
+        attempt(F, Retries - 1);
+    R ->
+        R
     end.
