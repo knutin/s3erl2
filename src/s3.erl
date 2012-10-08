d21abbfa6abf95ce2a3866c3989450c439531b8f
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2012-10-08 18:05:52 +0200

    Added some type specs.

--- src/s3.erl
+++ src/s3.erl
@@ -6,15 +6,42 @@
          delete/2, delete/3,
          stats/0]).
 
+-type value() :: string() | binary().
+-export_type([value/0]).
+
+-type header() :: {string(), string()}.
+
+-type bucket() :: string().
+-export_type([bucket/0]).
+
+-type key() :: string().
+-export_type([key/0]).
+
+-spec get(Bucket::bucket(), Key::key()) ->
+                 {ok, [ResponseHeaders::header()], Body::value()} |
+                 {ok, Body::value()} | term().
 get(Bucket, Key) ->
     get(Bucket, Key, 5000).
+
+-spec get(Bucket::bucket(), Key::key(), timeout()) ->
+                 {ok, [ResponseHeaders::header()], Body::value()} |
+                 {ok, Body::value()} | term().
 get(Bucket, Key, Timeout) ->
     call({request, {get, Bucket, Key}}, Timeout).
 
+-spec put(Bucket::bucket(), Key::key(), Value::value(),
+          ContentType::string()) ->
+                 {ok, Etag::any()} | ok | any().
 put(Bucket, Key, Value, ContentType) ->
     put(Bucket, Key, Value, ContentType, 5000).
+-spec put(Bucket::bucket(), Key::key(), Value::value(),
+          ContentType::string(), timeout()) ->
+                 {ok, Etag::any()} | ok | any().
 put(Bucket, Key, Value, ContentType, Timeout) ->
     put(Bucket, Key, Value, ContentType, Timeout, []).
+-spec put(Bucket::bucket(), Key::key(), Value::value(),
+          ContentType::string(), timeout(), list(header())) ->
+                 {ok, Etag::any()} | ok | any().
 put(Bucket, Key, Value, ContentType, Timeout, Headers) ->
     call({request, {put, Bucket, Key, Value, ContentType, Headers}}, Timeout).
 
